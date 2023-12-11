6ff79c114d27345370d78f8c582e17baff2ac98e
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-12-11 22:01:53 +0100

    first commit

---
+++ app/src/main/java/com/example/applicationdahhouelbazznajji/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.applicationdahhouelbazznajji;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+// Importations
+
+public class MainActivity extends AppCompatActivity {
+    Button bouton_explicite, bouton_implicite;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bouton_explicite=(Button) findViewById(R.id.button);
+        bouton_implicite=(Button) findViewById(R.id.button2);
+
+        bouton_explicite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getBaseContext(),DeuxiemeActivite.class);
+                intent.putExtra("nom","dahhou");
+                intent.putExtra("prénome","ilyas");
+                startActivity(intent);
+            }
+        });
+
+        bouton_implicite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.inpt.ac.ma"))
+                startActivity(intent);
+            }
+        });
+    }
+}
+
